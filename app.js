1db6d44d258ca40e805ca69d70265f76649f1f64
kieranwalker135 <73998046+kieranwalker135@users.noreply.github.com>
2021-01-11 16:07:19 +0000

    changed js file name

---
+++ app.js
@@ -0,0 +1,29 @@
+// let countDisplay = 0 // Stores the count number
+
+// function currentCounter(ammount){
+//     console.log("current count has changed");   
+//     document.getElementById('countDisplay').innerText = currentDisplay
+//     countDisplay = countDisplay + ammount
+// }
+
+// function increaseNumber(){
+//     console.log ("Number has increased by 1");
+//     countDisplay = countDisplay + 1;
+//     document.getElementById('countDisplay').innerText = countDisplay;
+// }
+
+// function decreaseNumber(){
+//     console.log ("Number has increased by 1");
+//     countDisplay = countDisplay - 1;
+//     document.getElementById('countDisplay').innerText = countDisplay;
+// }
+
+// function resetCounter(){
+//     console.log ("Number has been reset to 0");
+//     countDisplay = 0;
+//     document.getElementById('countDisplay').innerText = countDisplay;
+// }
+
+
+
+
